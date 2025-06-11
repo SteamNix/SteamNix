999aa245f3b44792a811ff9f9d046b0711f646d2
Leo Newton <leo.newton@icloud.com>
2025-06-10 18:37:27 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,15 +1,11 @@
 {
-  inputs.determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/*";
- # inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
+  inputs.chaotic.url = "https://flakehub.com/f/chaotic-cx/nyx/*.tar.gz";
  
-  outputs = { determinate, nixpkgs, chaotic, ... }: {
+  outputs = { nixpkgs, chaotic, ... }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        # Load the Determinate module
-        determinate.nixosModules.default
         ./configuration.nix
         chaotic.nixosModules.default
       ];
