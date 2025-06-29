b99ea9a9664d615d95d3d0f570fcd06b783dd09a
Leo Newton <leo.newton@icloud.com>
2025-06-29 08:11:48 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,5 +18,5 @@ nix-channel --update
 nixos-generate-config --root /mnt
 export NIX_CONFIG="experimental-features = flakes"
 export NIX_PATH="nixpkgs=/root/.nix-defexpr/channels/nixos"
-nixos-install --flake /etc/nixos/ --no-root-password
+nixos-install --flake /mnt/etc/nixos/flake.nix#nixos --no-root-password
 poweroff
