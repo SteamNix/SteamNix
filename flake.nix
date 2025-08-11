ab0d4ab930d3c791d878377e83699818ec80890a
Leo Newton <leo.newton@icloud.com>
2025-08-11 09:18:24 -0700

    Update Jovian

--- flake.nix
+++ flake.nix
@@ -1,13 +1,18 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
+  inputs.jovian = {
+    url = "github:Jovian-Experiments/Jovian-NixOS";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
  
-  outputs = { nixpkgs, chaotic, ... }: {
+  outputs = { nixpkgs, chaotic, jovian, ... }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
         chaotic.nixosModules.default
+        jovian.nixosModules.default
       ];
     };
   };
