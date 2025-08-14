9ff1fa1d5fa8a8b66a55025a44ab4cbb4c0cb95b
Leo Newton <leo.newton@icloud.com>
2025-08-13 18:39:20 -0700

    prevent screen from shutting off and power button shutsdown pc

--- configuration.nix
+++ configuration.nix
@@ -78,6 +78,10 @@
   # Graphical & Jovian   #
   ########################
   services.xserver.enable            = false;
+  #Power Button = Power Off
+  services.logind.extraConfig = ''HandlePowerKey=poweroff'';
+  #Prevents Screen from shutting off
+  services.xserver.xautolock.enable  = false;
   jovian = {
     steam.enable = true;
     steam.autoStart = true;
